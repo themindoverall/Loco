75191466ef128e652365570dc07aefe2019d078e
Chris Serino <themindoverall@gmail.com>
2013-07-16 19:29:20 -0400

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,4 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = 'precise64'
+	config.vm.network :forwarded_port, guest: 3000, host: 8080
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loco::Application.config.secret_token = '6b9dbd58841ad915e93e16cdaf1486cf6f9a908c831fb0714922c03f058adef9222dbbdd406f33ad3c9ea63965e6876e18e09a97e11c8a9c22189c1ef1890e53'
